fca75a8929be91121ad87dc0486d11ba534a06a9
David Sabatie <david.sabatie@notrenet.com>
2020-05-15 15:11:58 +0200

    update ui server

--- cmd/serveUi.go
+++ cmd/serveUi.go
@@ -17,7 +17,14 @@ package cmd
 
 import (
 	"fmt"
+	"io"
+	"log"
+	"net"
+	"net/http"
+	"text/template"
 
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/labstack/echo/v4"
 	"github.com/spf13/cobra"
 )
 
@@ -32,20 +39,89 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("serveUi called")
+		port, _ := cmd.Flags().GetInt("port")
+		e := echo.New()
+		indexBox, err := rice.FindBox("../../build")
+		if err != nil {
+			log.Fatal(err)
+		}
+		jsBox, err := rice.FindBox("../../build/bundle")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		templateString, err := indexBox.String("index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		// parse and execute the template
+		// tmplIndex, err := template.New("index").Parse(templateString)
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		box_ip, err := cmd.Flags().GetIP("box-ip")
+		if err != nil {
+			log.Fatal(err)
+		}
+		box_port, err := cmd.Flags().GetInt("box-port")
+		if err != nil {
+			log.Fatal(err)
+		}
+		box := &IndexTemplate{
+			DomopoolIP:   box_ip.String(),
+			DomopoolPort: box_port,
+		}
+		t := &Template{
+			templates: template.Must(template.New("index").Parse(templateString)),
+		}
+		e.Renderer = t
+		// bundle, err := jsBox.String("bundle.js")
+		// if err != nil {
+		// 	log.Fatal(err)
+		// }
+		// fmt.Println(bundle)
+		e.GET(
+			"/bundle/*",
+			echo.WrapHandler(
+				http.StripPrefix("/bundle/",
+					http.FileServer(
+						jsBox.HTTPBox(),
+					),
+				),
+			),
+		)
+		e.GET(
+			"/",
+			func(c echo.Context) error {
+				return c.Render(http.StatusOK, "index", box)
+			},
+		)
+
+		e.Logger.Fatal(
+			e.Start(
+				fmt.Sprintf(":%d", port),
+			),
+		)
 	},
+}
+
+type IndexTemplate struct {
+	DomopoolIP   string
+	DomopoolPort int
+}
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func init() {
 	rootCmd.AddCommand(serveUiCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// serveUiCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// serveUiCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	defaultIp := net.ParseIP("127.0.0.1")
+	serveUiCmd.Flags().IntP("port", "p", 8080, "port to listen on")
+	serveUiCmd.Flags().IPP("box-ip", "i", defaultIp, "ip of the domopool box")
+	serveUiCmd.Flags().Int("box-port", 80, "port of the domopool box")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/golgoth31/domopool-cli
 go 1.14
 
 require (
+	github.com/GeertJohan/go.rice v1.0.0
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
